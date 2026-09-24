0f9ab62fb0ed4b93c8e52ccc572c2291caeb7a33
Nur Alam <nuralam1122345@gmail.com>
2026-09-24 12:53:52 +0600

    fix: convert string id to number in getWorkoutById

--- src/app/workout/[id]/page.tsx
+++ src/app/workout/[id]/page.tsx
@@ -16,7 +16,7 @@ export default async function WorkoutDetailsPage({
 }: WorkoutDetailsPageProps) {
   const { id } = await params;
 
-  const workout = await getWorkoutById(id);
+  const workout = await getWorkoutById(Number(id));
 
   if (!workout) {
     notFound();
